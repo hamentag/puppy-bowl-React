cecef2474b314ec5a70fcfa906c78f6aa933260a
Hamza Amentag <hamentag1@gmail.com>
2024-05-12 20:29:07 -0500

    updated css styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,16 @@
+import { useState, useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
-import "./App.css";
+import { fetchAllPlayers } from "./api";
 import AllPlayers from "./components/AllPlayers";
 import SinglePlayer from "./components/SinglePlayer";
 import Teams from "./components/Teams";
 import Nav from "./components/Nav";
-
-
-import { useState, useEffect } from "react";
-import { fetchAllPlayers } from "./api";
+import "./App.css";
 
 function App() {
   const [players, setPlayers] = useState([]);
   const [numPlayers, setNumPlayers] = useState(0);
 
-  
   useEffect(() => {
     async function getAllPlayers() {
       const APIResponse = await fetchAllPlayers();
@@ -29,14 +26,18 @@ function App() {
 
   return (
     <div className="App">
-      <h1>Puppy Bowl</h1>
-      <Nav />
-      <Routes>
-        <Route path="/" element={<AllPlayers players={players} setPlayers={setPlayers} 
-          numPlayers={numPlayers} setNumPlayers={setNumPlayers}/>} />
-        <Route path="/:id" element={<SinglePlayer />} />
-        <Route path="/teams" element={<Teams players={players} />} />
-      </Routes>
+      <header>
+        <h1>Puppy Bowl</h1>
+        <Nav />
+      </header>
+      <main>
+        <Routes>
+          <Route path="/" element={<AllPlayers players={players} setPlayers={setPlayers} 
+            numPlayers={numPlayers} setNumPlayers={setNumPlayers}/>} />
+          <Route path="/:id" element={<SinglePlayer />} />
+          <Route path="/teams" element={<Teams players={players} />} />
+        </Routes>
+      </main>
     </div>
   )
 }
